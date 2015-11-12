1c546f8215c460522ccee163a7fffc80d1dd4742
liveu <liveu@naver.com>
2015-11-12 16:10:16 +0900

    app.js modified

--- password-manager/app.js
+++ password-manager/app.js
@@ -3,20 +3,42 @@ console.log("starting password-manager");
 var storage = require('node-persist');
 storage.initSync();
 
+var accounts = [];
+
 // account.name Facebook
 // account.username User12!
 // account.password Password123
 
 function createAccount(account){
     var accounts = storage.getItemSync("accounts");
-    //if accounts is undefined (use typeof)
-    //      set accounts = []
+    if(typeof accounts === "undefined"){
+        accounts = [];
+    }
 
-    // push on new account
-    // return account
+    accounts.push(account);
+    storage.setItemSync("accounts", accounts);
+
+    return accounts;
 }
 
 function getAccount(accountName){
     // var accounts use getItemSync
+    var accounts = storage.getItemSync("accounts");
+    var matchedAccount;
+
+    accounts.forEach(function(account){
+        if(account.name === accountName){
+            matchedAccount = account;
+        }
+    });
+    return matchedAccount;
 }
 
+//createAccount({
+//    name : 'Facebook',
+//    username : 'someemail@gmmai.com',
+//    password : 'Password123!'
+//});
+
+var facebookAccount = getAccount('Facebook');
+console.log(facebookAccount);
